105ff32011ff4f6a10d05667855ffdc3d582655c
oocheol <oocheol@naver.com>
2022-04-21 17:22:56 +0900

    22/04/21

--- src/main/java/model/MemberVO.java
+++ src/main/java/model/MemberVO.java
@@ -7,6 +7,35 @@ public class MemberVO {
 	private String pw;
 	private String nick;
 	
+	public MemberVO(String id, String pw, String nick) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.nick = nick;
+	}
+	
+	public MemberVO() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
 	
 	
 	
